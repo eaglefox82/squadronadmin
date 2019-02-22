af99a567cc9f2befebf7a159394222b43484005e
eaglefox82 <fox82b@gmail.com>
2019-02-22 16:26:27 +1100

    1

--- resources/views/roll/index.blade.php
+++ resources/views/roll/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header card-header-icon card-header-rose">
-                    <h4 class="card-title">Roll Date:</h4>
+                
                     <div class="pull-right new-button">
                         <a href="{{action('RollController@create')}}" class="btn btn-primary" title="Add Voucher"><i class="fa fa-plus fa-2x"></i>Create New Roll</a>
                     </div>
