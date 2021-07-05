31771a413877458f0d90bb65978c49f784a3ca6c
eaglefox82 <fox82b@gmail.com>
2021-07-05 23:04:31 +1000

    Route Update to support Refactor

--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,7 @@ Route::resource('/user', 'UsersController')->middleware('auth');
 
 
 Route::get('/activekids/voucher/{id}', 'ActiveKidsController@voucher')->middleware('auth');
-Route::get('/roll/paid/{id}', 'RollController@paid')->middleware('auth');
-Route::get('/roll/voucher/{id}', 'RollController@voucher')->middleware('auth');
-Route::get('/roll/notpaid/{id}', 'RollController@notpaid')->middleware('auth');
 Route::get('/roll/notpresent/{id}','RollController@notPresent')->middleware('auth');
-Route::get('/roll/online/{id}','RollController@online')->middleware('auth');
 Route::get('/roll/update/{id}/{status}', 'RollController@rollstatus')->middleware('auth');
 Route::get('/vouchers/complete/{id}', 'ActiveKidsController@complete')->middleware('auth');
 Route::get('/vouchers/submit/{id}', 'ActiveKidsController@submit')->middleware('auth');
